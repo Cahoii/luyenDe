2582e5b0861bf69dc7dc4cb760bc27bf6b6156a1
Cahoiidapoet <tangth023@gmail.com>
2026-03-31 22:37:27 +0700

    🔥

--- data/exams.js
+++ data/exams.js
@@ -1,7 +1,7 @@
 const EXAM_LIST = [
-    { id: "FA25_FE", name: "Đề thi FA25 FE" },
-    { id: "FA25_RE", name: "Đề thi FA25 RE" },
-    { id: "SP25_RE", name: "Đề thi SP25 RE" },
-    { id: "SU25_FE", name: "Đề thi SU25 FE" },
-    { id: "SU25_RE", name: "Đề thi SU25 RE" }
-];
+    { id: "FA25_FE", name: "Đề thi FA25 FE", subject: "MAI391" },
+    { id: "FA25_RE", name: "Đề thi FA25 RE", subject: "MAI391" },
+    { id: "SP25_RE", name: "Đề thi SP25 RE", subject: "MAI391" },
+    { id: "SU25_FE", name: "Đề thi SU25 FE", subject: "MAI391" },
+    { id: "SU25_RE", name: "Đề thi SU25 RE", subject: "MAI391" },
+]

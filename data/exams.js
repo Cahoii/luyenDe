6479e8ca6422fdcdf92c6986569914b0e5e6f0cd
Cahoiidapoet <tangth023@gmail.com>
2026-03-31 16:05:13 +0700

    👌

---
+++ data/exams.js
@@ -0,0 +1,7 @@
+const EXAM_LIST = [
+    { id: "FA25_FE", name: "Đề thi FA25 FE" },
+    { id: "FA25_RE", name: "Đề thi FA25 RE" },
+    { id: "SP25_RE", name: "Đề thi SP25 RE" },
+    { id: "SU25_FE", name: "Đề thi SU25 FE" },
+    { id: "SU25_RE", name: "Đề thi SU25 RE" }
+];
